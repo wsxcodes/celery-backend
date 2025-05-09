48900ff5c3d63cd318ae664af560ec674beb10b2
Jan Filips <janfilipsgt@gmail.com>
2025-05-09 19:55:55 +0300

    Fix import statement for utils_endpoints and ensure proper inclusion in the API router

--- backend/api/api_v1/routers.py
+++ backend/api/api_v1/routers.py
@@ -2,7 +2,7 @@ from fastapi import APIRouter
 
 from backend.api.api_v1.endpoints import (customer_endpoints,
                                           documents_endpoints,
-                                          generic_endpoints)
+                                          generic_endpoints, utils_endpoints)
 
 api_router = APIRouter()
 
@@ -19,9 +19,16 @@ api_router.include_router(
     tags=["Documents"]
 )
 
-# DB Endpoints
+# Customer Endpoints
 api_router.include_router(
     customer_endpoints.router,
     prefix="/customer",
     tags=["Customers"]
+)
+
+# Customer Endpoints
+api_router.include_router(
+    utils_endpoints.router,
+    prefix="/utils",
+    tags=["Utils"]
 )
