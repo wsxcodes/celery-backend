8f802bf3d7e41ec9fd2a40a2705fada8f906f6b4
Jan Filips <janfilipsgt@gmail.com>
2025-05-09 12:18:48 +0300

    Add initial configuration files for environment variables and API settings

---
+++ backend/config/__init__.py
@@ -0,0 +1,23 @@
+import os
+from pathlib import Path
+
+from dotenv import load_dotenv
+
+load_dotenv()
+
+CODE_DIR = os.path.dirname(__file__)
+ROOT_DIR = os.path.dirname(CODE_DIR)
+
+API_V1_STR = "/api/v1"
+
+DEVELOPMENT = os.getenv("DEVELOPMENT", None)
+
+SECRET_KEY = os.getenv("SECRET_KEY", "this-is-not-a-secret-key-make-it-secret")
+
+origins_str = os.getenv("CORS_ORIGINS", "")
+if origins_str:
+    CORS_ORIGINS = origins_str.split(",")
+else:
+    CORS_ORIGINS = []
+
+OPENAI_API_KEY = os.getenv("OPENAI_API_KEY", "MISSING-OPENAI_API_KEY")
