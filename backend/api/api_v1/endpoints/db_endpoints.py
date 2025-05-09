3e868ee9756af5b95a911e684f705c0a9223614d
Jan Filips <janfilipsgt@gmail.com>
2025-05-09 13:23:19 +0300

    Refactor db_endpoints.py to improve import organization and readability

--- backend/api/api_v1/endpoints/db_endpoints.py
+++ backend/api/api_v1/endpoints/db_endpoints.py
@@ -1,26 +1,21 @@
 import logging
-from typing import Dict
 from typing import List
-
-from backend.dependencies import get_db
 
 from fastapi import APIRouter, Depends
 
+from backend.db.schemas.files_schemas import FileRecord
 from backend.decorators import log_endpoint
+from backend.dependencies import get_db
 
 logger = logging.getLogger(__name__)
-
-router = APIRouter()
-
 
 # Ensure logging is properly configured
 logging.basicConfig(level=logging.INFO, format="%(asctime)s - %(levelname)s - %(message)s")
 logger = logging.getLogger(__name__)
 logger.setLevel(logging.INFO)
 
+router = APIRouter()
 
-from typing import List
-from backend.db.schemas.files_schemas import FileRecord
 
 @router.get("/list/{customer_id}", response_model=List[FileRecord])
 @log_endpoint
