7e8f648e9ea4c145f3504437694bb2ce0fafb32b
Jan Filips <janfilipsgt@gmail.com>
2025-05-09 14:14:30 +0300

    Update router to replace files_endpoints with documents_endpoints

--- backend/api/api_v1/routers.py
+++ backend/api/api_v1/routers.py
@@ -1,6 +1,6 @@
 from fastapi import APIRouter
 
-from backend.api.api_v1.endpoints import (db_endpoints, files_endpoints,
+from backend.api.api_v1.endpoints import (db_endpoints, documents_endpoints,
                                           generic_endpoints)
 
 api_router = APIRouter()
@@ -13,9 +13,9 @@ api_router.include_router(
 
 # Files Endpoints
 api_router.include_router(
-    files_endpoints.router,
-    prefix="/files",
-    tags=["Files"]
+    documents_endpoints.router,
+    prefix="/documents",
+    tags=["Documents"]
 )
 
 # DB Endpoints
