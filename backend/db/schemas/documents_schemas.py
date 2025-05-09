a6cbbec0c826527014b6f9099326407ed573e19f
Jan Filips <janfilipsgt@gmail.com>
2025-05-09 22:00:53 +0300

    Remove unused Customer model from documents_schemas.py

--- backend/db/schemas/documents_schemas.py
+++ backend/db/schemas/documents_schemas.py
@@ -4,11 +4,6 @@ from typing import Optional
 from pydantic import BaseModel
 
 
-class Customer(BaseModel):
-    customer_id: str
-    file_count: int
-
-
 class Document(BaseModel):
     id: int
     customer_id: str
