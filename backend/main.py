3725c6a4bda3ed589eea2bcb58e18fa901a8e6c6
Jan Filips <janfilipsgt@gmail.com>
2025-05-10 17:02:07 +0300

    Refactor imports and clean up unused code in main.py

--- backend/main.py
+++ backend/main.py
@@ -1,7 +1,5 @@
 import logging
-from datetime import datetime
 
-import humanize
 from fastapi import Depends, FastAPI, Request
 from fastapi.middleware.cors import CORSMiddleware
 from fastapi.responses import HTMLResponse, RedirectResponse
@@ -10,8 +8,8 @@ from fastapi.templating import Jinja2Templates
 from starlette.middleware.sessions import SessionMiddleware
 
 from backend import config
-from backend.api.api_v1.endpoints.customer_endpoints import (
-    add_new_customer, get_customer, list_customer_documents)
+from backend.api.api_v1.endpoints.customer_endpoints import (add_new_customer,
+                                                             get_customer)
 from backend.api.api_v1.routers import api_router
 from backend.decorators import log_endpoint
 from backend.dependencies import get_db, init_db
@@ -78,7 +76,6 @@ async def read_home(request: Request, db=Depends(get_db)):
         request.session["session_id"] = session_id
         logger.info("** Generated new session_id: %s", session_id)
         await add_new_customer(customer_id=session_id, output_language="Czech", db=db)
-
 
     customer = await get_customer(customer_id=session_id, db=db)
     logger.info("Customer data retrieved: %s", customer)
