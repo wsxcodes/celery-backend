127e331b5f32d425576c8c43a331ae055f54d034
Jan Filips <janfilipsgt@gmail.com>
2025-05-12 18:22:31 +0300

    Refactor code to comment out unused sections and add print statements for debugging output

--- xxx-openai.py
+++ xxx-openai.py
@@ -27,41 +27,52 @@ ai_client = AzureOpenAI(
 
 prompts = prompt_generators.load_prompts()
 
-document = safe_request(
-            request_type="GET",
-            url=config.API_URL + f"/api/v1/document/get/{document_uuid}",
-            data={},
-        )
-raw_text = document.json()["raw_text"]
+# document = safe_request(
+#             request_type="GET",
+#             url=config.API_URL + f"/api/v1/document/get/{document_uuid}",
+#             data={},
+#         )
+# raw_text = document.json()["raw_text"]
 
 output_language = "Slovak"
 tokens_spent = 0
 
 # -----------------------------------------------------------------------------------------------------------------------------
 
-# Run the smart summary prompt
-smart_summary = prompts["smart_summary"]
-data = run_ai_completition(ai_client=ai_client, prompt=smart_summary, document_text=raw_text, output_language="Slovak")
+# # Run the smart summary prompt
+# smart_summary = prompts["smart_summary"]
+# data = run_ai_completition(ai_client=ai_client, prompt=smart_summary, document_text=raw_text, output_language="Slovak")
+
+# usage = data.get("usage")
+# tokens_spent += usage["total_tokens"]
+
+# logger.info("Saving smart summary to database")
+# safe_request(
+#     request_type="PATCH",
+#     url=config.API_URL + f"/api/v1/document/metadata/{document_uuid}",
+#     data={
+#         "ai_category": data["top_category"],
+#         "ai_sub_category": data["sub_category"],
+#         "ai_summary_short": data["summary_short"],
+#         "ai_summary_long": data["summary_long"]
+#     }
+# )
+
+# # Record the tokens spent
+# update_tokens_spent(
+#     document_uuid=document_uuid,
+#     add_tokens_spent=tokens_spent
+# )
+
+# -----------------------------------------------------------------------------------------------------------------------------
+
+# Example simple prompt
+
+simple_prompt = prompts["example_prompt_simple"]
+data = run_ai_completition(ai_client=ai_client, prompt=simple_prompt, document_text="", output_language="Slovak")
 
 usage = data.get("usage")
 tokens_spent += usage["total_tokens"]
 
-logger.info("Saving smart summary to database")
-safe_request(
-    request_type="PATCH",
-    url=config.API_URL + f"/api/v1/document/metadata/{document_uuid}",
-    data={
-        "ai_category": data["top_category"],
-        "ai_sub_category": data["sub_category"],
-        "ai_summary_short": data["summary_short"],
-        "ai_summary_long": data["summary_long"]
-    }
-)
-
-# Record the tokens spent
-update_tokens_spent(
-    document_uuid=document_uuid,
-    add_tokens_spent=tokens_spent
-)
-
-# -----------------------------------------------------------------------------------------------------------------------------
+print(data)
+print(usage)
