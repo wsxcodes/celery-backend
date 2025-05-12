8bd6152b8ef5edd8b9e5df9d462b3d01bc80fd50
Jan Filips <janfilipsgt@gmail.com>
2025-05-12 14:59:40 +0300

    Add logging setup and improve smart summary processing flow

--- xxx-openai.py
+++ xxx-openai.py
@@ -1,11 +1,16 @@
 import os 
 import json
+import logging
 from backend import config
 from backend.utils import prompt_generators
 from backend.utils.prompt_generators import run_ai_completition
 from backend.utils.helpers import safe_request
 
 from openai import AzureOpenAI
+
+logging.basicConfig(level=logging.INFO)
+
+logger = logging.getLogger(__name__)
 
 
 ai_client = AzureOpenAI(
@@ -28,11 +33,20 @@ raw_text = document.json()["raw_text"]
 output_language = "Slovak"
 
 
-
-example_prompt = prompts["smart_summary"]
-
-
-data = run_ai_completition(ai_client=ai_client, prompt=example_prompt, document_text=raw_text, output_language="Slovak")
+smart_summary = prompts["smart_summary"]
+data = run_ai_completition(ai_client=ai_client, prompt=smart_summary, document_text=raw_text, output_language="Slovak")
 
 from pprint import pprint
 pprint(data)
+
+logger.info("Saving smart summary to database")
+safe_request(
+    request_type="PATCH",
+    url=config.API_URL + f"/api/v1/document/metadata/{document_uuid}",
+    data={
+        "ai_category": data["top_category"],
+        "ai_sub_category": data["sub_category"],
+        "ai_summary_short": data["summary_short"],
+        "ai_summary_long": data["summary_long"]
+    }
+)
