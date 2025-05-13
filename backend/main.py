e013bede6b8b0b24a26488210fd4df8a777530d3
Jan Filips <janfilipsgt@gmail.com>
2025-05-14 00:18:45 +0300

    Remove categories endpoint and associated HTML template for simplification

--- backend/main.py
+++ backend/main.py
@@ -97,12 +97,6 @@ async def read_home(request: Request, db=Depends(get_db)):
     )
 
 
-@app.get("/categories", response_class=HTMLResponse, include_in_schema=False)
-@log_endpoint
-async def read_categories(request: Request, db=Depends(get_db)):
-    return templates.TemplateResponse("categories.html", {"request": request})
-
-
 @app.get("/404", response_class=HTMLResponse, include_in_schema=False)
 @log_endpoint
 async def read_404(request: Request, db=Depends(get_db)):
