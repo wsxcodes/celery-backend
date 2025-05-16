24a00bce370455015c331fc606c17b38195ff6cd
Jan Filips <janfilipsgt@gmail.com>
2025-05-16 10:40:08 +0300

    Fix: Add TODO for pedantic mode in analysis_worker.py

--- workers/analysis_worker.py
+++ workers/analysis_worker.py
@@ -189,7 +189,7 @@ def main():
                         url=config.API_URL + f"/api/v1/document/{document_uuid}",
                         data={},
                     )
-            
+
             # XXX TODO pedantic mode (!!)
 
             features_and_insights = prompts["alerts_and_actions"]
